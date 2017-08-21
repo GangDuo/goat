88939035d63ecc9275d711f05eceb65683869eec
GangDuo <onoda-y-xyz@mve.biglobe.ne.jp>
2017-08-21 07:52:13 +0000

    draft

--- ui.R
+++ ui.R
@@ -11,6 +11,7 @@ library(shiny)
 library(config)
 library(DBI)
 library(RMySQL)
+library(DT)
 
 db.driver <- dbDriver("MySQL")
 db.connector <- dbConnect(db.driver,
@@ -20,11 +21,43 @@ db.connector <- dbConnect(db.driver,
 #dbGetQuery(db.connector, "SET NAMES cp932") # if windows
 dbGetQuery(db.connector, "SET NAMES utf8")
 
+stores <- dbGetQuery(db.connector, config::get("qstores"))
+categories <- dbGetQuery(db.connector, config::get("qcategories"))
+division <- dbGetQuery(db.connector, config::get("qdivision"))
+family <- dbGetQuery(db.connector, config::get("qfamily"))
+
 dbDisconnect(db.connector)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
+  headerPanel("年間週別売上金額推移"),
+  
+  verticalLayout(
+    # 週間売上推移
+    plotOutput("plot_weekly_sales_trends", height = "300px"),
 
-
+    sidebarLayout(
+      position = "left",
+      sidebarPanel(
+        selectInput("category", "カテゴリ", categories, NULL, TRUE),
+        selectInput("division", "小分類", division, NULL, TRUE),
+        selectInput("family", "棚名", family, NULL, TRUE),
+        selectInput("store", "店舗:", stores, NULL, TRUE),
+        sliderInput("year", "年:", 15, 20, 17)
+      ),
+      
+      mainPanel(
+        splitLayout(
+          tabsetPanel(
+            tabPanel("週間売上金額推移", DT::dataTableOutput("tbl_weekly_sales_trends")),
+            tabPanel(textOutput("tab_title_top20"), DT::dataTableOutput("tbl_top20")),
+            tabPanel(textOutput("tab_title_bottom20"), DT::dataTableOutput("tbl_bottom20")),
+            tabPanel("選択行", DT::dataTableOutput('selected'))
+          )
+        )
+      )
+    )
+  )
+  
 ))
