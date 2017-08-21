d1a4a8209f2f5645e2e8182baee3ca9a7114e91f
GangDuo <onoda-y-xyz@mve.biglobe.ne.jp>
2017-08-21 07:11:54 +0000

    use MySQL.

--- ui.R
+++ ui.R
@@ -8,6 +8,19 @@
 #
 
 library(shiny)
+library(config)
+library(DBI)
+library(RMySQL)
+
+db.driver <- dbDriver("MySQL")
+db.connector <- dbConnect(db.driver,
+                          dbname=config::get("dbname"),
+                          user=config::get("user"), password=config::get("password"),
+                          host=config::get("host"), port=config::get("port")) 
+#dbGetQuery(db.connector, "SET NAMES cp932") # if windows
+dbGetQuery(db.connector, "SET NAMES utf8")
+
+dbDisconnect(db.connector)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
